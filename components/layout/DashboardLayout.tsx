c6600c0560dea04b2b51cd1dc79bde623689a87c
Zayk22 <azeezol547@gmail.com>
2026-05-23 11:55:08 +0100

    Fix dark mode text visibility

--- components/layout/DashboardLayout.tsx
+++ components/layout/DashboardLayout.tsx
@@ -21,7 +21,7 @@ export default function DashboardLayout({
       <div className="flex-1 flex flex-col w-full max-w-full">
         <Header onMenuClick={() => setSidebarOpen(true)} />
         
-        <main className="flex-1 overflow-y-auto p-4 lg:p-8 w-full max-w-full">
+        <main className="flex-1 overflow-y-auto p-4 lg:p-8 w-full max-w-full text-gray-900 dark:text-gray-100">
           {children}
         </main>
       </div>
